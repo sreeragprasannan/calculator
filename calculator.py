9b8a40c38b58dbd51603bee5b0c1794d9087317d
sreerag <sreeraaag@gmail.com>
2023-07-20 22:19:26 +0530

    working base model

--- calculator.py
+++ calculator.py
@@ -4,8 +4,29 @@ window = Tk()
 window.geometry("316x334")  # window size
 window.title("Calculator")
 
+expression = ""
+
+# this function is used to take values from the button for calculation
+# displays the input value on the input field
+def btn_click(item):
+    global expression
+    expression = expression + str(item)
+    input_text.set(expression)
+
+# to clear the input field
+def btn_clear():
+    global expression
+    expression = ""
+    input_text.set("")
+
+def btn_equals():
+    global expression
+    result = str(eval(expression))
+    input_text.set(result)
+    expression = ""
 
 input_text = StringVar()
+
 # display frame
 input_frame = Frame(window, width=310, height=50, bd=0, highlightbackground="black", highlightcolor="black", highlightthickness=2)
 input_frame.pack(side=TOP)
@@ -19,39 +40,33 @@ input_field.pack(ipady=10)
 btns_frame = Frame(window, width=312, height=272.5, bg="grey")
 btns_frame.pack()
 
-# buttons
-one = Button(btns_frame, text="1", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-two = Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-three = Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-four = Button(btns_frame, text="4", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-five = Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-six = Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-seven = Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-nine = Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-zoro = Button(btns_frame, text="0", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-multiplication = Button(btns_frame, text="*", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-division = Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-substraction = Button(btns_frame, text="-", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-add = Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
-equals = Button(btns_frame, text="=", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2")
+# ===================button formate========================
+# first row
+clear = Button(btns_frame, text="C", fg="black", width=32, height=3, bd=0, bg="#eee", cursor="hand2", command=lambda:btn_clear()).grid(row=0, column=0, columnspan=3, padx=1, pady=1)
+division = Button(btns_frame, text="/", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click("/")).grid(row=0, column=3, padx=1, pady=1)
 
-# buttons alignment
-seven.grid(row=0, column=0)
-eight.grid(row=0, column=1)
-nine.grid(row=0, column=2)
-division.grid(row=0,column=3)
-four.grid(row=1, column=0)
-five.grid(row=1, column=1)
-six.grid(row=1, column=2)
-multiplication.grid(row=1,column=3)
-one.grid(row=2, column=0)
-two.grid(row=2, column=1)
-three.grid(row=2, column=2)
-substraction.grid(row=2,column=3)
-zoro.grid(row=3, column=1)
-equals.grid(row=3,column=2)
-add.grid(row=3,column=3)
+# second row
+seven = Button(btns_frame, text="7", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(7)).grid(row=1, column=0, padx=1, pady=1)
+eight = Button(btns_frame, text="8", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(8)).grid(row=1, column=1, padx=1, pady=1)
+nine = Button(btns_frame, text="9", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(9)).grid(row=1, column=2, padx=1, pady=1)
+multiplication = Button(btns_frame, text="*", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click("*")).grid(row=1, column=3, padx=1, pady=1)
+
+# third row
+four = Button(btns_frame, text="4", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(4)).grid(row=2, column=0, padx=1, pady=1)
+five = Button(btns_frame, text="5", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(5)).grid(row=2, column=1, padx=1, pady=1)
+six = Button(btns_frame, text="6", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(6)).grid(row=2, column=2, padx=1, pady=1)
+substraction = Button(btns_frame, text="-", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click("-")).grid(row=2, column=3, padx=1, pady=1)
+
+# fourth row
+one = Button(btns_frame, text="1", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(1)).grid(row=3, column=0, padx=1, pady=1)
+two = Button(btns_frame, text="2", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(2)).grid(row=3, column=1, padx=1, pady=1)
+three = Button(btns_frame, text="3", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(3)).grid(row=3, column=2, padx=1, pady=1)
+add = Button(btns_frame, text="+", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click("+")).grid(row=3, column=3, padx=1, pady=1)
+
+# fifth row
+point = Button(btns_frame, text=".", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(".")).grid(row=4, column=0, padx=1, pady=1)
+zoro = Button(btns_frame, text="0", fg="black", width=10, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_click(0)).grid(row=4, column=1, padx=1, pady=1)
+equals = Button(btns_frame, text="=", fg="black", width=21, height=3, bd=0, bg="#fff", cursor="hand2", command=lambda:btn_equals()).grid(row=4, column=2, columnspan=2, padx=1, pady=1)
 
 
 window.mainloop()
